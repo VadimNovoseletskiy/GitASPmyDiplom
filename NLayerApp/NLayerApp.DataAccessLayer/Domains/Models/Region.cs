a4fa4be7382b8c6bab1c46fc6e94ac3898e56c57
Master <ttm121212@gmail.com>
2017-09-07 13:25:20 +0300

    Add model(class Region).

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Region.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.DataAccessLayer.Domains.Models
+{
+    /// <summary>
+    /// Describes the areas where the property is located   
+    /// </summary>
+    /// 
+    public class Region
+    {
+            /// <summary>
+            /// The property Id 
+            /// </summary>
+            public int Id { get; set; }
+            /// <summary>
+            /// The property RegionName 
+            /// </summary>
+            ///<value>
+            /// The district name  
+            /// </value>
+            public string RegionName { get; set; }
+
+            //Links:
+            /// <summary>
+            /// The navigation property.
+            /// The link to the class"Info" (one to many communication
+            /// </summary> 
+            public ICollection<Info> Infos { get; set; }
+            public Region()
+            {
+                Infos = new List<Info>();
+            }
+        }
+    
+}
