79f4b660727e7c568f546d913235380e04c9762e
Master <ttm121212@gmail.com>
2017-09-07 13:38:56 +0300

    Add Interface"IDbContext".

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Interface/IDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.DataAccessLayer.Interface
+{
+    public interface IDbContext:IDisposable
+    {
+        IDbSet<T> Set<T>()
+            where T : class;
+
+        int SaveChanges();
+        DbEntityEntry Entry(object o);
+        DbContextTransaction BeginTransaction();
+
+    }
+}
