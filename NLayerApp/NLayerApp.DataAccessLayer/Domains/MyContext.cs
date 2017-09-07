aeb22914c993602913d7f42a4aca89226a74db4b
Master <ttm121212@gmail.com>
2017-09-07 15:21:55 +0300

    Add class MyContext.

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/MyContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+
+namespace NLayerApp.DataAccessLayer.Domains
+{
+    public class MyContext:DbContext,IDbContext
+    {
+
+
+        /// <summary>
+        /// The Constructor
+        /// </summary>
+        public MyContext() : base("MyConnection")
+        {
+            /*To disable lazy loading for all objects in the context,
+            Set its configuration property to false*/
+            this.Configuration.LazyLoadingEnabled = false;
+            /*To improve performance,
+            turn off detection of changes*/
+            this.Configuration.AutoDetectChangesEnabled = false;
+        }
+
+        /// <summary>
+        /// The DbSets
+        /// The properties that will receive a data Set or Get from the database 
+        /// </summary>
+        public DbSet<Info> Infos { get; set; }
+        public DbSet<Village> Villages { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+
+        /// <inheritdoc/>
+        public new IDbSet<T> Set<T>()
+            where T : class => base.Set<T>();
+        /// <inheritdoc/>
+        public DbContextTransaction BeginTransaction() => this.Database.BeginTransaction();
+
+    }
+}
