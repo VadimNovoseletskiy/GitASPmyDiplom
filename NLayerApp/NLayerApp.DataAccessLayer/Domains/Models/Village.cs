005869a8c652d16cd67991d2ff9781101fb29f09
Master <ttm121212@gmail.com>
2017-09-07 13:24:59 +0300

    Add model(class Village)

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Village.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.DataAccessLayer.Domains.Models
+{
+    public class Village
+    {
+        /// <summary>
+        /// The properties Id 
+        /// </summary>
+        /// <value>
+        /// The Id 
+        /// </value>
+        public int Id { get; set; }
+        /// <summary>
+        /// The properties VillageName 
+        /// </summary>
+        /// <value>
+        /// Name of the village    
+        /// </value>
+        public string VillageName { get; set; }
+
+        //Links:
+        /// <summary>
+        /// The navigation property.
+        /// The link to the class "Info"(one to many communication)
+        /// </summary>
+        public ICollection<Info> Infos { get; set; }
+        public Village()
+        {
+            Infos = new List<Info>();
+        }
+    }
+}
