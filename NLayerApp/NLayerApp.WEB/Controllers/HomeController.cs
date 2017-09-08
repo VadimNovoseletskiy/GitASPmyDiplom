4670f98f42c93c4c04d12e7d95e8d49d2d3749bf
Master <ttm121212@gmail.com>
2017-09-09 00:47:53 +0300

    Add MyContext in the HomeController.

--- NLayerApp/NLayerApp.WEB/Controllers/HomeController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/HomeController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLayerApp.DataAccessLayer.Domains;
+using NLayerApp.DataAccessLayer.Domains.Models;
 
 namespace NLayerApp.WEB.Controllers
 {
     public class HomeController : Controller
     {
+        MyContext dbContext=new MyContext();
         public ActionResult Index()
         {
+            IQueryable<Village> villages = dbContext.Villages;
+            ViewBag.vill = villages;
             return View();
         }
 
