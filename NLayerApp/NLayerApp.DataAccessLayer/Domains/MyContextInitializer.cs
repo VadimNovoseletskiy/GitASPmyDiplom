87d708c761a11fc50cd5dd59b66e800edcee4c32
Master <ttm121212@gmail.com>
2017-09-08 18:09:15 +0300

    Add initialization Village, Region, Info

--- NLayerApp/NLayerApp.DataAccessLayer/Domains/MyContextInitializer.cs
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/MyContextInitializer.cs
@@ -18,11 +18,38 @@ namespace NLayerApp.DataAccessLayer.Domains
             Village v2 = new Village { VillageName = "VillageName_2" };
             Village v3 = new Village { VillageName = "VillageName_3" };
 
+            dbContext.Villages.AddRange(new List<Village> {v0, v1, v2, v3 });
 
+            //add region
+            Region r0 = new Region { RegionName = "RegionName_0" };
+            Region r1 = new Region { RegionName = "RegionName_1" };
+            dbContext.Regions.AddRange(new List<Region> {r0, r1});
 
-            //add region 
-            //Region r0=new Region {RegionName = "RegionName_0" };
-            //Region r1 = new Region { RegionName = "RegionName_1" };
+            //add info
+            Info i0=new Info
+            {
+                NameInfo = "Продам 1 но кімнатну квартиру ",
+                DetailsInfo = " Квартира знаходиться у 10 секції на 4 поверсі, на стадії будівництва було перепланування у якому була збільшена кухня",
+                Type = 1,
+                DollarPrice = 3400000,
+                GrnPrice = 101001093,
+                TotalAreaInfo = 123,
+                Village= v0,
+                Region = r1
+            };
+            Info i1=new Info
+            {
+                NameInfo = "Продам 2 во кімнатну квартиру ",
+                DetailsInfo = "Продам гарну 2 кімнатну квартиру в новобуді ЖК Європейський квартал 9 секця 64.99 м кв по 8450 за 1м кв",
+                Type = 1,
+                DollarPrice = 3500000,
+                GrnPrice = 202001093,
+                TotalAreaInfo = 356,
+                Village = v2,
+                Region = r1
+            };
+
+            dbContext.Infos.AddRange(new List<Info> {i1, i0});
 
             //add wall materials
             //WallMaterial w0=new WallMaterial {NameWallMaterils = "NameWallMaterils_0" };
@@ -34,7 +61,7 @@ namespace NLayerApp.DataAccessLayer.Domains
             //FloorMaterial fm1 = new FloorMaterial { NameFloorMaterils = "NameFloorMaterils_1" };
             //FloorMaterial fm2 = new FloorMaterial { NameFloorMaterils = "NameFloorMaterils_2" };
 
-            dbContext.Villages.Add(v0);
+
 
             dbContext.SaveChanges();
         }
