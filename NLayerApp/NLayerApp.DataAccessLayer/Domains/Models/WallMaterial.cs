7fc17ab1543429948f4e73c7e941a3689d5e6b57
Master <ttm121212@gmail.com>
2017-09-09 20:13:47 +0300

    Add model(class"WallMaterial") in the domain.model.

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/WallMaterial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.DataAccessLayer.Domains.Models
+{
+    /// <summary>
+    /// Description of material for walls
+    /// </summary>
+    public class WallMaterial
+    {
+        /// <summary>
+        /// The property Id 
+        /// </summary>
+        /// <value>
+        /// The Id 
+        /// </value>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// The property NameWallMaterils 
+        /// </summary>
+        /// <value>
+        /// The name of the material for the walls 
+        /// </value>
+        public string NameWallMaterils { get; set; }
+    }
+}
