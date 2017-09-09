85eaf547b74db22b15d844010eb46da70a2ff7cb
Master <ttm121212@gmail.com>
2017-09-09 20:14:27 +0300

    Add model(class"FloorMaterial") in the domain.model.

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/FloorMaterial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.DataAccessLayer.Domains.Models
+{
+    /// <summary>
+    /// Description of the material on the floor 
+    /// </summary>
+    public class FloorMaterial
+    {
+        /// <summary>
+        /// The property Id 
+        /// </summary>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// The property NameFloorMaterils
+        /// </summary>
+        ///<value>
+        /// Describe : Material name
+        /// </value>
+        public string NameFloorMaterils { get; set; }
+
+    }
+}
