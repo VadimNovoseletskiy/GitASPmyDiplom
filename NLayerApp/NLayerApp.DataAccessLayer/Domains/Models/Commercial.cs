e8d1a8509063fc01cbda9ba3ba91f4760da96826
Master <ttm121212@gmail.com>
2017-09-10 23:02:40 +0300

    Add model(class"Commercial") in the domain.model.

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Commercial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.DataAccessLayer.Domains.Models
+{
+    /// <summary>
+    /// Describe an object"Commercial property"  
+    /// </summary>
+    public class Commercial
+    {
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// Property TypeCommersial
+        /// </summary>
+        /// <value>
+        /// Commercial type 
+        /// </value>
+        public string TypeCommercial { get; set; }
+
+
+        /// <summary>
+        /// Property StateCommersial
+        /// </summary>
+        /// <value>
+        /// State  
+        /// </value>
+        public string StateCommercial { get; set; }
+
+
+        /// <summary>
+        /// Property FloorCommersial
+        /// </summary>
+        /// <value>
+        /// Floor where is the commercial real estate
+        /// </value>
+        public int FloorCommercial { get; set; }
+
+
+        /// <summary>
+        /// Property TotalFloorCommersial
+        /// </summary>
+        /// <value>
+        /// Number of floors
+        /// </value>
+        public int TotalFloorCommercial { get; set; }
+
+
+        /// <summary>
+        /// Property EffectiveAreaCommmersial
+        /// </summary>
+        /// <value>
+        /// Effective area  
+        /// </value>
+        public float EffectiveAreaCommercial { get; set; }
+
+
+        /// <summary>
+        /// Property LandAreaCommersial
+        /// </summary>
+        /// <value>
+        /// Area of adjacent land 
+        ///  </value>
+        public float LandAreaCommercial { get; set; }
+
+    }
+}
