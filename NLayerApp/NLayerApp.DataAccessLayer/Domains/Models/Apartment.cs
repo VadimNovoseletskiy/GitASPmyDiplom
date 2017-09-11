28c37f7f018282ddfbd60d740ca7d86d3cea5ed8
Master <ttm121212@gmail.com>
2017-09-11 16:42:22 +0300

    Fix syntax error in model(class Apartment).

--- NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Apartment.cs
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Apartment.cs
@@ -100,9 +100,6 @@ namespace NLayerApp.DataAccessLayer.Domains.Models
         /// </summary>
         public Info Info { get; set; }
 
-        /// <summary>
-        /// The  link to the Child Class"Additional Equipment" (one to one communication ) 
-        /// </summary>
-        public AdditionalEquipment AdditionalEquipment { get; set; }
+        
     }
 }
