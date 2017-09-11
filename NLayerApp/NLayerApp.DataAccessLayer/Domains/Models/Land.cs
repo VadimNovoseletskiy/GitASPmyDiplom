1e7548879f496e458891e1e79bbff3832c3c0878
Master <ttm121212@gmail.com>
2017-09-11 12:16:47 +0300

    Add model(class"Land") in the domain.model.

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Land.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.DataAccessLayer.Domains.Models
+{
+    /// <summary>
+    /// Describes an object"Land"
+    /// </summary>
+    public class Land
+    {
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// The property SpesialLand
+        /// </summary>
+        ///<value>
+        /// Describe special Land
+        /// </value>
+        public string SpecialLand { get; set; }
+
+
+        /// <summary>
+        /// The property LandArea
+        /// </summary>
+        /// <value>
+        /// Land plot area
+        /// </value>
+        public float LandArea { get; set; }
+
+
+    }
+}
