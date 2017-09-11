4b6277187334a7fdc84c9e638ff559c8ec260863
Master <ttm121212@gmail.com>
2017-09-11 20:12:48 +0300

    Add "one to one communication" model(Apartment-Material).

--- NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Material.cs
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Material.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,11 @@ namespace NLayerApp.DataAccessLayer.Domains.Models
 
     public class Material
     {
+        /// <summary>
+        /// The  Foreign Key(one to one communication). Link to the Parent  class "Apartment"
+        /// </summary>
+        [Key]
+        [ForeignKey("Apartment")]
         public int Id { get; set; }
 
         /// <summary>
@@ -31,6 +38,9 @@ namespace NLayerApp.DataAccessLayer.Domains.Models
         /// </value>
         public string NameFloorMaterial { get; set; }
 
+
+        //Links:
+
         /// <summary>
         /// The navigation property.
         /// The link to the class "WallMaterial"(one to many communication)
@@ -48,6 +58,11 @@ namespace NLayerApp.DataAccessLayer.Domains.Models
             WallMaterials = new List<WallMaterial>();
             FloorMaterials=new List<FloorMaterial>();
         }
+
+        /// <summary>
+        /// The link to the parent class Apartment(one to one communication)
+        /// </summary>
+        public Apartment Apartment { get; set; }
 
 
     }
