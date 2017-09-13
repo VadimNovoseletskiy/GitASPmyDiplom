eeab65f6d5dbeafc53f598607df05817f5155470
Master <ttm121212@gmail.com>
2017-09-13 23:49:24 +0300

    Add class "AdditionalEquipmentDataTransferObject" (model data transfer layer).

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/DataTransferObject/AdditionalEquipmentDataTransferObject.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BusinessLogicLayer.DataTransferObject
+{
+    public class AdditionalEquipmentDataTransferObject
+    {
+        /// <summary>
+        /// Property Id 
+        /// </summary>
+        public int Id { get; set; }
+
+
+        /// <summary >
+        /// Property Boiler 
+        /// </summary>
+        /// <value>
+        /// Additional Equipment: Boiler 
+        /// </value>
+        public bool BoilerAdditionalEquipment { get; set; }
+
+
+        /// <summary>
+        /// Property Intercom
+        /// </summary>
+        /// <value>
+        /// AdditionalEquipment :Intercom
+        /// </value>
+        public bool IntercomAdditionalEquipment { get; set; }
+
+
+        /// <summary>
+        /// Property Internet
+        /// </summary>
+        /// <value>
+        /// Additional Equipment : Internet
+        /// </value>
+        public bool InternetAdditionalEquipment { get; set; }
+
+
+        /// <summary>
+        /// Property Cable TV
+        /// </summary>
+        ///<value>
+        /// Additional Equipment : Cable TV
+        /// </value>
+        public bool CableTVadditionalEquipment { get; set; }
+
+
+        /// <summary>
+        /// Property Fireplace 
+        /// </summary>
+        /// <value>
+        /// Additional Equipment : Fireplace 
+        /// </value>
+        public bool FirePlaceAdditionalEquipment { get; set; }
+
+
+        /// <summary>
+        /// Property Air Condition
+        /// </summary>
+        /// <value>
+        /// Additional Equipment : Air Condition 
+        /// </value>
+        public bool AirConditioningAdditionalEquipment { get; set; }
+
+
+        /// <summary>
+        /// Property Signaling
+        /// </summary>
+        /// <value>
+        /// Additional Equipment : Signaling 
+        /// </value>
+        public bool SignalingAdditionalEquipment { get; set; }
+
+
+        /// <summary>
+        /// Property Satellite TV
+        /// </summary>
+        /// <value>
+        /// Additional Equipment :Satellite TV
+        /// </value>
+        public bool SatelliteTVadditionalEquipment { get; set; }
+
+
+        /// <summary>
+        /// Property Double glazed windows
+        /// </summary>
+        /// <value>
+        /// Additional Equipment : Additional Equipment :
+        /// </value>
+        public bool WindowsAdditionalEquipment { get; set; }
+
+
+        /// <summary>
+        /// Property Telephone 
+        /// </summary>
+        /// <value>
+        /// Additional Equipment :Telephone  
+        /// </value>
+        public bool TelephoneAdditionalEqipment { get; set; }
+    }
+}
