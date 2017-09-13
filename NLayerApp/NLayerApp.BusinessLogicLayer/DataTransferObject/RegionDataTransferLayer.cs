c9a24b2304a10bfb9d698ecf08fe85a8dddc4158
Master <ttm121212@gmail.com>
2017-09-13 23:17:19 +0300

    Add class RegionDataTransferLayer (model data transfer layer).

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/DataTransferObject/RegionDataTransferLayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BusinessLogicLayer.DataTransferObject
+{
+    /// <summary>
+    /// The Region
+    /// </summary>
+    public class RegionDataTransferLayer
+    {
+        /// <summary>
+        /// The property Id 
+        /// </summary>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// The property RegionName 
+        /// </summary>
+        ///<value>
+        /// The district name  
+        /// </value>
+        public string RegionName { get; set; }
+
+    }
+}
