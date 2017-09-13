7972f181c62b43f0c11e9b1bfb35edb30912fea3
Master <ttm121212@gmail.com>
2017-09-13 19:03:17 +0300

    Add class"LandDataTransferObject"(model data transfer object).

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/DataTransferObject/LandDataTransferObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BusinessLogicLayer.DataTransferObject
+{
+    public class LandDataTransferObject
+    {
+        /// <summary>
+        /// Property Id
+        /// </summary>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// The property SpesialLand
+        /// </summary>
+        ///<value>
+        /// Describe special Land
+        /// </value>
+        public string SpecialLand { get; set; }
+
+
+        /// <summary>
+        /// The property LandArea
+        /// </summary>
+        /// <value>
+        /// Land plot area
+        /// </value>
+        public float LandArea { get; set; }
+    }
+}
