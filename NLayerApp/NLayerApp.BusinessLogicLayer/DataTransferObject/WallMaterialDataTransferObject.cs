a020fa497f33138682b61e5ecc5f622ce9d60083
Master <ttm121212@gmail.com>
2017-09-14 00:13:01 +0300

    Add class  "WallMaterialDataTransferObject" (model data transfer object).

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/DataTransferObject/WallMaterialDataTransferObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BusinessLogicLayer.DataTransferObject
+{
+    public class WallMaterialDataTransferObject
+    {
+        /// <summary>
+        /// The property Id 
+        /// </summary>
+        /// <value>
+        /// The Id 
+        /// </value>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// The property NameWallMaterils 
+        /// </summary>
+        /// <value>
+        /// The name of the material for the walls 
+        /// </value>
+        public string NameWallMaterils { get; set; }
+    }
+}
