7b14e74dcf65fca35cb8074073c78a5dae1be449
Master <ttm121212@gmail.com>
2017-09-14 13:22:27 +0300

    Add class RegionHandler

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Handler/RegionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.BusinessLogicLayer.Handler
+{
+    public class RegionHandler
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public RegionHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public RegionHandler()
+            : this(new UnitOfWork())
+        {
+        }
+
+    }
+}
