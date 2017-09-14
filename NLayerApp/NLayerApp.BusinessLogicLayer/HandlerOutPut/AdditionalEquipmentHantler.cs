0b4ea856f2b375331e4fe5a3f44685c874ddf21a
Master <ttm121212@gmail.com>
2017-09-14 13:49:38 +0300

    Add class "AdditionalEquipmentHantler"

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/HandlerOutPut/AdditionalEquipmentHantler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.BusinessLogicLayer.HandlerOutPut
+{
+    public class AdditionalEquipmentHantler
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public AdditionalEquipmentHantler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public AdditionalEquipmentHantler() : this(new UnitOfWork())
+        {
+
+        }
+    }
+}
