5c35bb936d3468716fc39e73b1242e85446740a7
Master <ttm121212@gmail.com>
2017-09-15 00:04:55 +0300

    Add class "LandViewModel".

---
+++ NLayerApp/NLayerApp.WEB/Models/LandViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BusinessLogicLayer.DataTransferObject
+{
+    /// <summary>
+    /// Describes an object"Land"
+    /// </summary>
+    public class LandViewModel
+    {
+        
+        public int Id { get; set; }
+
+        public string SpecialLand { get; set; }
+
+        public float LandArea { get; set; }
+    }
+}
