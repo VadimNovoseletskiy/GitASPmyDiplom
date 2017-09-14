4ac44af0a6ca530dfdafd690ae5aa66b45aeb7ac
Master <ttm121212@gmail.com>
2017-09-14 12:39:24 +0300

    Add classes "NameViewModel" in the folder "Models".

---
+++ NLayerApp/NLayerApp.WEB/Models/LandViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BusinessLogicLayer.DataTransferObject
+{
+    /// <summary>
+    /// Describes an object"Land"
+    /// </summary>
+    public class LandDataTransferLayer
+    {
+        /// <summary>
+        /// The property Id 
+        /// </summary>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// The property SpesialLand
+        /// </summary>
+        ///<value>
+        /// Describe special Land
+        /// </value>
+        public string SpecialLand { get; set; }
+
+
+        /// <summary>
+        /// The property LandArea
+        /// </summary>
+        /// <value>
+        /// Land plot area
+        /// </value>
+        public float LandArea { get; set; }
+    }
+}
