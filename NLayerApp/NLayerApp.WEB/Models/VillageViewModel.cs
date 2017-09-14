9782c1d4d48f0def3f1f15fc097a3059437047a0
Master <ttm121212@gmail.com>
2017-09-14 18:09:14 +0300

    Add class (model)NameViewModel in the folder Models.

---
+++ NLayerApp/NLayerApp.WEB/Models/VillageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.WEB.Models
+{
+    /// <summary>
+    /// The Village 
+    /// </summary>
+    public class VillageViewModel
+    {
+        /// <summary>
+        /// The properties Id 
+        /// </summary>
+        /// <value>
+        /// The Id 
+        /// </value>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// The properties VillageName 
+        /// </summary>
+        /// <value>
+        /// Name of the village    
+        /// </value>
+        public string VillageName { get; set; }
+    }
+}
