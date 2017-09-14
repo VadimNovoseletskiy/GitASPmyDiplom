298b40b04f40123d93f0a8b57b45e676fd1d01df
Master <ttm121212@gmail.com>
2017-09-15 00:14:05 +0300

    Delete comments.

--- NLayerApp/NLayerApp.WEB/Models/CommercialViewModel.cs
+++ NLayerApp/NLayerApp.WEB/Models/CommercialViewModel.cs
@@ -11,63 +11,19 @@ namespace NLayerApp.WEB.Models
     /// </summary>
     public class CommercialViewModel
     {
-        /// <summary>
-        /// Property Id 
-        /// </summary>
+        
         public int Id { get; set; }
 
-
-        /// <summary>
-        /// Property TypeCommersial
-        /// </summary>
-        /// <value>
-        /// Commercial type 
-        /// </value>
         public string TypeCommercial { get; set; }
 
-
-        /// <summary>
-        /// Property StateCommersial
-        /// </summary>
-        /// <value>
-        /// State  
-        /// </value>
         public string StateCommercial { get; set; }
 
-
-        /// <summary>
-        /// Property FloorCommersial
-        /// </summary>
-        /// <value>
-        /// Floor where is the commercial real estate
-        /// </value>
         public int FloorCommercial { get; set; }
 
-
-        /// <summary>
-        /// Property TotalFloorCommersial
-        /// </summary>
-        /// <value>
-        /// Number of floors
-        /// </value>
         public int TotalFloorCommercial { get; set; }
 
-
-        /// <summary>
-        /// Property EffectiveAreaCommmersial
-        /// </summary>
-        /// <value>
-        /// Effective area  
-        /// </value>
         public float EffectiveAreaCommercial { get; set; }
 
-
-        /// <summary>
-        /// Property LandAreaCommersial
-        /// </summary>
-        /// <value>
-        /// Area of adjacent land 
-        ///  </value>
         public float LandAreaCommercial { get; set; }
     }
 }
