b9aa99b579bcb30a991535fcb7f1322d9bdeee84
Master <ttm121212@gmail.com>
2017-09-20 14:26:17 +0300

    Add class "NameHandlerOutPut".

---
+++ NLayerApp/NLayerApp.WEB/BusinessLogicLayer/HandlerOutPut/HouseHandlerOutPut.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.BusinessLogicLayer.Handler
+{
+    public class HouseHandlerOutPut
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public HouseHandlerOutPut(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public HouseHandlerOutPut() : this(new UnitOfWork())
+        {
+            
+        }
+    }
+}
---
+++ NLayerApp/NLayerApp.WEB/BusinessLogicLayer/HandlerOutPut/LandHandlerOutPut.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.BusinessLogicLayer.Handler
+{
+    public class LandHandlerOutPut
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public LandHandlerOutPut(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public LandHandlerOutPut() : this(new UnitOfWork())
+        {
+            
+        }
+    }
+}
