8b4f4efafa5f59f07fa8f696d7c086f2030c5560
Master <ttm121212@gmail.com>
2017-10-04 23:12:21 +0300

    Add class "LandViewModels" in WEB layer

---
+++ NLayerApp/NLayerApp.WEB/Models/LandViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NLayerApp.WEB.Models
+{
+    public class LandViewModels
+    {
+        //ссылка-краткий заголовок
+        public string NameInfo { get; set; }
+        public string VillageRegion { get; set; }
+        public float TotalAreaInfo { get; set; }
+        public int GrnPrice { get; set; }
+        public int DollarPrice { get; set; }
+    }
+}
