e2dc6f1a6006d7ffe9267c19522904c1cfd42813
Master <ttm121212@gmail.com>
2017-10-04 23:28:18 +0300

    Add method void GetInformation in LandHandler.

--- NLayerApp/NLayerApp.BusinessLogicLayer/Handler/LandHandlerOutPut.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Handler/LandHandlerOutPut.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Domains.Models;
 using NLayerApp.DataAccessLayer.Interface;
 using NLayerApp.DataAccessLayer.Repository;
+
 
 namespace NLayerApp.BusinessLogicLayer.Handler
 {
@@ -20,6 +23,21 @@ namespace NLayerApp.BusinessLogicLayer.Handler
         public LandHandlerOutPut() : this(new UnitOfWork())
         {
             
+        }
+
+        void GetInformation(LandSearchParameters parameters)
+        {
+            this.unitOfWork.GenericRepository<Info>().Get().Where(p => p.Type == PropertyType.Land
+                                                            && p.Region.RegionName==parameters.VillageRegion        
+                                                            || p.Land.SpecialLand == SpecialTypeLand.Commercial
+                                                            || p.Land.SpecialLand == SpecialTypeLand.Agricultural
+                                                            || p.Land.SpecialLand == SpecialTypeLand.Individual
+                                                            && p.DollarPrice > parameters.CostFrom
+                                                            && p.DollarPrice< parameters.CostTo
+                                                            ).Select(IQueryable<>)
+                                                          
+
+
         }
     }
 }
