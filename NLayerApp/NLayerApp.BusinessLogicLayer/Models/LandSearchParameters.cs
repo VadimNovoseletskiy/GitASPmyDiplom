cee20cf0a109000117b411b3ccbdc8681d7f857d
Master <ttm121212@gmail.com>
2017-10-04 23:13:28 +0300

    Add class "LandSearchParameters".

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/LandSearchParameters.cs
@@ -0,0 +1,21 @@
+﻿namespace NLayerApp.BusinessLogicLayer.Models
+{
+    /// <summary>
+    /// Describes an object"Land"
+    /// </summary>
+    public class LandSearchParameters
+    {
+
+        public string SpecialType { get; set; }
+
+        public string VillageRegion { get; set; }
+
+        public string SpecialLand { get; set; }
+
+        public float LandArea { get; set; }
+
+        public int CostFrom { get; set; }
+
+        public int CostTo { get; set; }
+    }
+}
