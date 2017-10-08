3b3bccc2024e71e2d16a9a4dd9eb82e43471c8c6
Master <ttm121212@gmail.com>
2017-10-08 19:50:45 +0300

    Add method "GetInformation" in LandController.

--- NLayerApp/NLayerApp.WEB/Controllers/LandController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/LandController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLayerApp.BusinessLogicLayer.Handler;
+using NLayerApp.BusinessLogicLayer.Models;
 
 namespace NLayerApp.WEB.Controllers
 {
     public class LandController : Controller
     {
         // GET: Land
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        
+        //POST:Land 
+        [HttpPost]
+        public ActionResult Index(LandSearchParameters parameters)
+        {
+            LandHandlerOutPut myLandHandlerOutPut = new LandHandlerOutPut();
+            var resultLand = myLandHandlerOutPut.GetInformation(parameters);
+            return View(resultLand);
+          
         }
     }
 }
