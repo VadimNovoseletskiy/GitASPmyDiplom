141f7cfc609c49c7232c1ebe171370ef25122157
Master <ttm121212@gmail.com>
2017-10-08 10:57:31 +0300

    Transfer class LandViewModels from WEB layer to Business layer.

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/LandViewModels.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/LandViewModels.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace NLayerApp.WEB.Models
+﻿namespace NLayerApp.BusinessLogicLayer.Models
 {
     public class LandViewModels
     {
-        //ссылка-краткий заголовок
         public string NameInfo { get; set; }
-        public string VillageRegion { get; set; }
+        public string Region { get; set; }
+        public string Village{ get; set; }
         public float TotalAreaInfo { get; set; }
         public int GrnPrice { get; set; }
         public int DollarPrice { get; set; }
