db35467e561e0093404b618703b498ac692c178e
Master <ttm121212@gmail.com>
2017-10-12 13:49:24 +0300

    Delete property "GrnPrice" and add properties "Id", "CaptionLink".

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/LandViewModel.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/LandViewModel.cs
@@ -2,11 +2,12 @@
 {
     public class LandViewModel
     {
+        public int Id { get; set; }
+        public string CaptionLink { get; set; }
         public string NameInfo { get; set; }
         public string Region { get; set; }
         public string Village{ get; set; }
         public float TotalAreaInfo { get; set; }
-        public int GrnPrice { get; set; }
         public int DollarPrice { get; set; }
     }
 }
