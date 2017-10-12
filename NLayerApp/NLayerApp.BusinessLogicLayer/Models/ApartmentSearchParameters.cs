8d894a57e02817d79c9872bfb7b9d9f7bad7fa22
Master <ttm121212@gmail.com>
2017-10-12 16:39:53 +0300

    Add class"ApartmentSearchParameters"(receives data(parameters of search) from  Apartment search form).

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/ApartmentSearchParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Domains.Models;
+
+namespace NLayerApp.BusinessLogicLayer.Models
+{
+    public class ApartmentSearchParameters
+    {
+        public int Region { get; set; }
+        public int Village { get; set; }
+        public SpecialTypeRoom TypeRoom { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int DollarPrice { get; set; }
+        public int GrnPrice { get; set; }
+    }
+}
