afd9b929a76b48cbc099284d3da9cbb1f67d5213
Master <ttm121212@gmail.com>
2017-10-12 13:43:19 +0300

    Add "VillageHandler" and "RegionHandler" in project.

---
+++ NLayerApp/NLayerApp.WEB/Handler/RegionHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+using NLayerApp.WEB.Models;
+
+namespace NLayerApp.WEB.Handler
+{
+    public class RegionHandler
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public RegionHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public RegionHandler() : this(new UnitOfWork())
+        {
+
+        }
+
+        public List<DropDownViewModel> GetRegion()
+        {
+
+            List<DropDownViewModel> result= this.unitOfWork.GenericRepository<Region>()
+                .Get()
+                .Select(
+                    p => new DropDownViewModel
+                    {
+                        Id = p.Id,
+                        Value = p.RegionName
+                    }
+                )
+                .ToList<DropDownViewModel>();
+            return result;
+        }
+    }
+}
---
+++ NLayerApp/NLayerApp.WEB/Handler/VillageHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+using NLayerApp.WEB.Models;
+
+namespace NLayerApp.WEB.Handler
+{
+    public class VillageHandler
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public VillageHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public VillageHandler() : this(new UnitOfWork())
+        {
+        }
+
+
+
+        public List<DropDownViewModel> GetVillage() =>
+            this.unitOfWork.GenericRepository<Village>()
+                .Get()
+                .Select(
+                    p => new DropDownViewModel
+                    {
+                        Id = p.Id,
+                        Value = p.VillageName
+                    }
+                )
+                .ToList();
+    }
+}
