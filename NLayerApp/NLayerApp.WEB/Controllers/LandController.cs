533e2a7f2f0615d97476ae6b86a1ba3b4b5dd47f
Master <ttm121212@gmail.com>
2017-10-12 14:28:01 +0300

    Work on the LandController.

--- NLayerApp/NLayerApp.WEB/Controllers/LandController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/LandController.cs
@@ -7,30 +7,54 @@ using NLayerApp.BusinessLogicLayer.Handler;
 using NLayerApp.BusinessLogicLayer.Models;
 using NLayerApp.DataAccessLayer.Domains;
 using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+using RegionHandler = NLayerApp.WEB.Handler.RegionHandler;
+using VillageHandler = NLayerApp.WEB.Handler.VillageHandler;
 
 namespace NLayerApp.WEB.Controllers
 {
    
     public class LandController : Controller
     {
-        MyContext dbContext=new MyContext();//test out put
+        //MyContext dbContext=new MyContext();//test out put
+        IUnitOfWork unitOfWork=new UnitOfWork();
+
         // GET: Land
         [HttpGet]
         public ActionResult Index()
         {
-            IQueryable<Land> lands = dbContext.Land;//test out put
-            ViewBag.land = lands;//test out put
-            return View();
+           MySelect();
+
+           
+            return View(/*new List<LandViewModel>()*/);
         }
         
         //POST:Land 
         [HttpPost]
         public ActionResult Index(LandSearchParameters parameters)
         {
-            LandHandlerOutPut myLandHandlerOutPut = new LandHandlerOutPut();
+            MySelect();
+
+            LandHandlerOutPut myLandHandlerOutPut = new LandHandlerOutPut(unitOfWork);
+
             var resultLand = myLandHandlerOutPut.GetInformation(parameters);
             return View(resultLand);
           
+        }
+
+        void MySelect()
+        {
+            RegionHandler myRegionHandler = new RegionHandler(unitOfWork);
+            var resultRegion = myRegionHandler.GetRegion();
+            SelectList listRegion = new SelectList(resultRegion, "Id", "Value");
+            ViewBag.regionName = listRegion;
+
+            VillageHandler myVillageHandler = new VillageHandler(unitOfWork);
+            var resultVillage = myVillageHandler.GetVillage();
+            SelectList listVillage = new SelectList(resultVillage, "Id", "Value");
+            ViewBag.villageName = listVillage;
+
         }
     }
 }
