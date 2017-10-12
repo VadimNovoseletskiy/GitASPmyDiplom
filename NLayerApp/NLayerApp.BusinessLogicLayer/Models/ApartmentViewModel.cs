413d329ac74c2094a04aebb0fa9aeb3b5744bb72
Master <ttm121212@gmail.com>
2017-10-12 16:07:28 +0300

    Add class "ApartmentViewModel"Model of output of search results by the user of the Apartment).

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/ApartmentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BusinessLogicLayer.Models
+{
+     public  class ApartmentViewModel
+    {
+         public int Id { get; set; }
+         public string CaptionLink { get; set; }
+         public string Village{ get; set; }
+         public string Region { get; set; }
+         public int Floor { get; set; }
+         public int NumberOfRooms { get; set; }
+         public float TotalAreaInfo { get; set; }
+         public int DollarPrice { get; set; }
+         public int GrnPrice { get; set; }
+
+    }
+}
