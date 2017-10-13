1d63cdbc26de9405df3885a8555d193f4f44a98f
Master <ttm121212@gmail.com>
2017-10-13 22:39:01 +0300

    Add in CommercialController method"MySelect".

--- NLayerApp/NLayerApp.WEB/Controllers/CommercialController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/CommercialController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using NLayerApp.DataAccessLayer.Interface;
 using NLayerApp.DataAccessLayer.Repository;
+using NLayerApp.WEB.Handler;
 
 namespace NLayerApp.WEB.Controllers
 {
@@ -16,13 +17,28 @@ namespace NLayerApp.WEB.Controllers
         [HttpGet]
         public ActionResult Index()
         {
+            MySelect();
             return View();
         }
 
         [HttpPost]
         public ActionResult Index()
         {
+            MySelect();
             return View();
+        }
+
+        void MySelect()
+        {
+            RegionHandler myRegionHandler=new RegionHandler(unitOfWork);
+            var resultRegion = myRegionHandler.GetRegion();
+            SelectList listRegion=new SelectList(resultRegion,"Id","Value");
+            ViewBag.regionName = listRegion;
+
+            VillageHandler myVillageHandler=new VillageHandler(unitOfWork);
+            var resultVillage = myVillageHandler.GetVillage();
+            SelectList listVillage=new SelectList(resultVillage,"Id", "Value");
+            ViewBag.villageName = listVillage;
         }
     }
 }
