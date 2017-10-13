ceec830da6ee2ff9f512747b62c8fa7a198b5bce
Master <ttm121212@gmail.com>
2017-10-13 21:28:44 +0300

    Add properties in the class "CommercialViewModel".

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/CommercialViewModel.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/CommercialViewModel.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Domains.Models;
 
 namespace NLayerApp.BusinessLogicLayer.Models
 {
     public class CommercialViewModel
     {
-
+        public string Region { get; set; }
+        public string Village { get; set; }
+        public string StateCommercial { get; set; }
+        public float TotalArea { get; set; }
+        public int DollarPrice{ get; set; }
+        public int GrnPrice{ get; set; }
     }
 }
