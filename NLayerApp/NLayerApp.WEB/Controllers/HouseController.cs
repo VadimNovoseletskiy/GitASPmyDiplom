bd073ed4c52e3c6c0c82194904b9e48e18832c15
Master <ttm121212@gmail.com>
2017-10-13 16:40:42 +0300

    Add method "MySelect" in HouseController. This Select list for field "Region" and "Village".

--- NLayerApp/NLayerApp.WEB/Controllers/HouseController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/HouseController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+using NLayerApp.WEB.Handler;
 
 namespace NLayerApp.WEB.Controllers
 {
     public class HouseController : Controller
     {
+        IUnitOfWork unitOfWork=new UnitOfWork();
+
         // GET: House
         public ActionResult Index()
         {
+            MySelect();
             return View();
         }
 
@@ -18,7 +24,21 @@ namespace NLayerApp.WEB.Controllers
         //POST:House(form) 
         public ActionResult Index()
         {
+            MySelect();
             return View();
+        }
+
+        void MySelect()
+        {
+            RegionHandler myRegionHandler=new RegionHandler(unitOfWork);
+            var resultRegion = myRegionHandler.GetRegion();
+            SelectList listRegion=new SelectList(resultRegion,"Id","Value");
+            ViewBag.regionName = listRegion;
+
+            VillageHandler myVillageHandler=new VillageHandler(unitOfWork);
+            var resultVillage = myVillageHandler.GetVillage();
+            SelectList listVillage=new SelectList(resultVillage, "Id", "Value");
+            ViewBag.villageName = listVillage;
         }
     }
 }
