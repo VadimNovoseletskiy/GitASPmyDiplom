58e4d2f7a1547b6eeb534f60674612a97473f0ec
Master <ttm121212@gmail.com>
2017-10-13 22:00:18 +0300

    Add property"NameInfo" in class "CommercialViewModel" and working on the CommercialHandlerOutPut.

--- NLayerApp/NLayerApp.BusinessLogicLayer/Handler/CommercialHandlerOutPut.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Handler/CommercialHandlerOutPut.cs
@@ -3,11 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
 
 namespace NLayerApp.BusinessLogicLayer.Handler
 {
     public class CommercialHandlerOutPut
     {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CommercialHandlerOutPut(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public CommercialHandlerOutPut() : this(new UnitOfWork())
+        {
+
+        }
+
+        public List<CommercialViewModel> GetCommercial(CommercialSearchParameters parameters) =>
+            this.unitOfWork.GenericRepository<Info>()
+                .Get()
+                .Where(p => p.Type == PropertyType.Commercial
+                            && p.VillageId == parameters.Village
+                            && p.RegionId == parameters.Region
+                            && p.OperationType == parameters.TypeOperation
+                            && p.Commercial.TypeCommercial == parameters.TypeCommercial
+                            && p.DollarPrice >= parameters.DollarPriceFrom
+                            && p.DollarPrice <= parameters.DollarPriceTo
+                            && p.GrnPrice >= parameters.GrnPriceFrom
+                            && p.GrnPrice <= parameters.DollarPriceTo
+
+                )
+                .Select(p => new CommercialViewModel
+                {
+                    Id = p.Id,
+                    Village = p.Village.VillageName,
+                    Region = p.Region.RegionName,
+                    StateCommercial = p.Commercial.StateCommercial,
+                    TotalArea = p.TotalAreaInfo,
+                    DollarPrice = p.DollarPrice,
+                    GrnPrice = p.GrnPrice
+                }
+
+                )
+                .ToList<CommercialViewModel>();
+
 
     }
 }
