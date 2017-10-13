719bb886f1c9e6b438538cef607bdaeddbc6f5f8
Master <ttm121212@gmail.com>
2017-10-13 13:39:10 +0300

    Add class"HouseViewModel".

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/HouseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Domains.Models;
+
+namespace NLayerApp.BusinessLogicLayer.Models
+{
+    public class HouseViewModel
+    {
+        public int Id { get; set; }
+        public string CaptionLint { get; set; }
+        public string Village{ get; set; }
+        public string Region { get; set; }
+        public SpecialTypeHouse TypeHouse { get; set; }
+        public int Floor { get; set; }
+        public float TotalArea { get; set; }
+
+    }
+}
