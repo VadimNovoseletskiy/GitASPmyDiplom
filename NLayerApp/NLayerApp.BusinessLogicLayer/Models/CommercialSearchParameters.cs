429a161621a3638c632603b77fe585b3413301cb
Master <ttm121212@gmail.com>
2017-10-13 21:17:17 +0300

    Add properties in the class "CommercialSearchParameters".

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/CommercialSearchParameters.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/CommercialSearchParameters.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Domains.Models;
 
 namespace NLayerApp.BusinessLogicLayer.Models
 {
     public class CommercialSearchParameters
     {
-
+        public int Region { get; set; }
+        public int Village { get; set; }
+        public TypeOfOperation TypeOperation { get; set; }
+        public string TypeCommercial { get; set; }
+        public int DollarPriceFrom { get; set; }
+        public int DollarPriceTo { get; set; }
+        public int GrnPriceFrom { get; set; }
+        public int GrnPriceTo { get; set; }
     }
 }
