d74eb7cbd67668d3f6ba56226fa5497d157923b0
Master <ttm121212@gmail.com>
2017-10-13 12:27:43 +0300

    Add HouseController in WEB project.

---
+++ NLayerApp/NLayerApp.WEB/Controllers/HouseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NLayerApp.WEB.Controllers
+{
+    public class HouseController : Controller
+    {
+        // GET: House
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //POST:House(form) 
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
