ff5e8c38b9d46f4534f23081374b1370ae084715
Master <ttm121212@gmail.com>
2017-10-13 22:28:57 +0300

    Add object "UnitOfWork" in CommercialController.

--- NLayerApp/NLayerApp.WEB/Controllers/CommercialController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/CommercialController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
 
 namespace NLayerApp.WEB.Controllers
 {
     public class CommercialController : Controller
     {
+        IUnitOfWork unitOfWork=new UnitOfWork();
 
         // GET: Commercial
         [HttpGet]
