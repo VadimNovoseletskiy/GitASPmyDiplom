32bf15534174b551a4e1d1d28afb315b390e19ab
Master <ttm121212@gmail.com>
2017-10-13 13:28:26 +0300

    Add class "HouseSearchParameters".Describes object form search House.

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/HouseSearchParameters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Domains.Models;
+
+namespace NLayerApp.BusinessLogicLayer.Models
+{
+    public class HouseSearchParameters
+    {
+        public int Region { get; set; }
+        public int Village { get; set; }
+        public TypeOfOperation OperationType { get; set;}
+        public SpecialTypeHouse TypeHouse { get; set; }
+        public SpecialPartHouse PartHouse { get; set; }
+        public int DollarPriceFrom { get; set; }
+        public int DollarPriceTo { get; set; }
+        public int GrnPriceFrom { get; set; }
+        public int GrnPriceTo { get; set; }
+
+    }
+}
