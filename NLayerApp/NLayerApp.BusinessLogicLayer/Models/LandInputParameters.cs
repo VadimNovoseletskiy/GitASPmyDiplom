8cccb347a546e6f656cd61febecf73c0dba8231e
Master <ttm121212@gmail.com>
2017-10-14 23:53:33 +0300

    Add in class "LandInputParameter" property (enum) Type(Land).Add comment on checkBox (Communication).

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/LandInputParameters.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/LandInputParameters.cs
@@ -9,7 +9,8 @@ namespace NLayerApp.BusinessLogicLayer.Models
 {
     public class LandInputParameters
     {
-//Address
+        public PropertyType Type = PropertyType.Land;
+        //Address
         public string Village { get; set; }
         public string Region { get; set; }
         public string Street { get; set; }
@@ -19,12 +20,12 @@ namespace NLayerApp.BusinessLogicLayer.Models
         public SpecialTypeLand TypeLand { get; set; }
         public float LandArea { get; set; }
 //communications
-        public bool Gas { get; set; }
-        public bool RailWay { get; set; }
-        public bool CentralSewerage { get; set; }
-        public bool CentralWater { get; set; }
-        public bool CentralHeating { get; set; }
-        public bool Electricity { get; set; }
+        //public bool Gas { get; set; }
+        //public bool RailWay { get; set; }
+        //public bool CentralSewerage { get; set; }
+        //public bool CentralWater { get; set; }
+        //public bool CentralHeating { get; set; }
+        //public bool Electricity { get; set; }
 //Describes object
         public string CaptionLink { get; set; }
         public string NameInfo { get; set; }
