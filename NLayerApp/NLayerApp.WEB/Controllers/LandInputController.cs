b2ea1100d874ee02080da7325de3ce54dda09e6e
Master <ttm121212@gmail.com>
2017-10-14 13:30:12 +0300

    Add class "LandInputController "

---
+++ NLayerApp/NLayerApp.WEB/Controllers/LandInputController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NLayerApp.WEB.Controllers
+{
+    public class LandInputController : Controller
+    {
+        // GET: LandInput
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
