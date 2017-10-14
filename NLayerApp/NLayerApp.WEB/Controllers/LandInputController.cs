f9445b1ec66ce4fbb1b90959b36d607267c768f5
Master <ttm121212@gmail.com>
2017-10-15 00:09:06 +0300

    Working on the LandInputController.

--- NLayerApp/NLayerApp.WEB/Controllers/LandInputController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/LandInputController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLayerApp.BusinessLogicLayer.Handler;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
 
 namespace NLayerApp.WEB.Controllers
 {
     public class LandInputController : Controller
     {
+        IUnitOfWork unitOfWork=new UnitOfWork();
         // GET: LandInput
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Index(LandInputParameters parameters)
+        {
+            LandHandlerInput myHandlerInput=new LandHandlerInput(unitOfWork);
+            myHandlerInput.InsertLand(parameters);
+            myHandlerInput.SaveObject();
+            return View();
+
         }
     }
 }
