ae83e9d331fc65dd2bdba93d4dbfc7ec2b39bca4
Master <ttm121212@gmail.com>
2017-10-14 13:47:41 +0300

    Add class "CommercialInputController".

---
+++ NLayerApp/NLayerApp.WEB/Controllers/CommercialInputController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NLayerApp.WEB.Controllers
+{
+    public class CommercialInputController : Controller
+    {
+        // GET: CommercialInput
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
