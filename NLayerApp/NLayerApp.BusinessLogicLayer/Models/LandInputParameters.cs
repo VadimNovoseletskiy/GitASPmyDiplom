a501b09e18b3269b27c1610b1b256412d859585f
Master <ttm121212@gmail.com>
2017-10-14 17:24:08 +0300

    Work on the comparison of classes"LandInputParametrs" and Index(LandInputController).

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/LandInputParameters.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/LandInputParameters.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Domains.Models;
 
 namespace NLayerApp.BusinessLogicLayer.Models
 {
-    class LandInputParameters
+    public class LandInputParameters
     {
+//Address
+        public string Village { get; set; }
+        public string Region { get; set; }
+        public string Street { get; set; }
+        public string NumberAdress { get; set; }
+//land information
+        public TypeOfOperation OperationType { get; set; }
+        public SpecialTypeLand TypeLand { get; set; }
+        public float LandArea { get; set; }
+//communications
+        public bool Gas { get; set; }
+        public bool RailWay { get; set; }
+        public bool CentralSewerage { get; set; }
+        public bool CentralWater { get; set; }
+        public bool CentralHeating { get; set; }
+        public bool Electricity { get; set; }
+//Describes object
+        public string CaptionLink { get; set; }
+        public string NameInfo { get; set; }
+        public string InfoDetails { get; set; }
+        public string InfoPrivat { get; set; }
+//Money
+        public int GrnPrice { get; set; }
+        public int DollarPrice { get; set; }
     }
 }
