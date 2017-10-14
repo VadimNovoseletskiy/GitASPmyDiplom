d11c76c332ca47ab495312dd3616b58522ff8d7d
Master <ttm121212@gmail.com>
2017-10-14 15:51:29 +0300

    Add class "LendHandlerInput" in Busines Layer.

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Handler/LandHandlerInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.BusinessLogicLayer.Handler
+{
+    public class LandHandlerInput
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public LandHandlerInput(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public LandHandlerInput() : this(new UnitOfWork())
+        {
+        }
+    }
+}
