d35c470c142a2ef57103ddeb3e197cf893b47058
Master <ttm121212@gmail.com>
2017-10-15 20:56:08 +0300

    Work on class"CommercialInputController".

--- NLayerApp/NLayerApp.WEB/Controllers/CommercialInputController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/CommercialInputController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLayerApp.BusinessLogicLayer.Handler;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
 
 namespace NLayerApp.WEB.Controllers
 {
     public class CommercialInputController : Controller
     {
+        IUnitOfWork unitOfWork=new UnitOfWork();
+
         // GET: CommercialInput
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+        
+        //POST
+        [HttpPost]
+        public ActionResult Index(CommercialInputParameters parameters)
+        {
+            CommercialHandlerInput myHandlerInput=new CommercialHandlerInput(unitOfWork);
+            myHandlerInput.InsertCommercial(parameters);
+            myHandlerInput.SaveObject();
+            return View();
+        }
+
     }
 }
