51872b2e47a1f0793a976476dbe12454fbe9f9fb
Master <ttm121212@gmail.com>
2017-10-15 16:58:26 +0300

    Work on "HouseInputController".

--- NLayerApp/NLayerApp.WEB/Controllers/HouseInputController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/HouseInputController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLayerApp.BusinessLogicLayer.Handler;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
 
 namespace NLayerApp.WEB.Controllers
 {
     public class HouseInputController : Controller
     {
+        IUnitOfWork unitOfWork=new UnitOfWork();
         // GET: HouseInput
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Index(HouseInputParameters parameters)
+        {
+            HouseHandlerInput myHandlerInput=new HouseHandlerInput(unitOfWork);
+            myHandlerInput.InsertHouse(parameters);
+            myHandlerInput.SaveObject();
             return View();
         }
     }
