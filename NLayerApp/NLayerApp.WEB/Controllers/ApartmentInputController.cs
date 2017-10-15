f454febc8ca1ef560324bed80650815e31ab274a
Master <ttm121212@gmail.com>
2017-10-15 19:54:12 +0300

    Work on class"ApartmentInputControllers".

--- NLayerApp/NLayerApp.WEB/Controllers/ApartmentInputController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/ApartmentInputController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLayerApp.BusinessLogicLayer.Handler;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
 
 namespace NLayerApp.WEB.Controllers
 {
     public class ApartmentInputController : Controller
     {
+        IUnitOfWork unitOfWork = new UnitOfWork();
+
         // GET: ApartmentInput
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        //POST
+        [HttpPost]
+        public ActionResult Index(ApartmentInputParameters parameters)
+        {
+            ApartmentHandlerInput myHandlerInput = new ApartmentHandlerInput(unitOfWork);
+            myHandlerInput.InsertApartment(parameters);
+            myHandlerInput.SaveObject();
+            return View();
+        }
+
+
     }
 }
