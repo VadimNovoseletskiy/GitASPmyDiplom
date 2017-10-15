f2b1705b262ceb8c083247fbd2febfa4133f737c
Master <ttm121212@gmail.com>
2017-10-15 19:27:37 +0300

    Work on class"ApartmentInputParameters"

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/ApartmerntInputParameters.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/ApartmerntInputParameters.cs
@@ -18,6 +18,47 @@ namespace NLayerApp.BusinessLogicLayer.Models
         public TypeOfOperation OperationType { get; set; }
 
         //Details Info  
+        public string ConditionOfApartment { get; set;}
+        public int RoomsApartment { get; set; }
+        public SpecialTypeRoom RoomType { get; set; }
+        public string ReadinessApartment { get; set; }
+        public string BathRoomApartment { get; set; }
+        public int BalconyApartment { get; set; }
+        public int FloorApartment { get; set; }
+        public int TotalFloor { get; set; }
+        //материал пола
+        //материал стен
+        public float TotalArea { get; set; }
+        public float LivingAreaApartment { get; set; }
+        public float KitchenAreaApartment { get; set; }
+        
+        
+        
+        
+        
+        //Additional Equipment 
+        //public bool Boiler { get; set; }
+        //public bool Intercom { get; set; }
+        //public bool Internet { get; set; }
+        //public bool CableTv { get; set; }
+        //public bool FirePlace { get; set; }
+        //public bool Air { get; set; }
+        //public bool Furniture { get; set; }
+        //public bool Signaling { get; set; }
+        //public bool SatelliteTv{ get; set; }
+        //public bool MyWindows { get; set; }
+        //public bool Telephone { get; set; }
+        //public bool WashingMachine { get; set; }
+
+        //Describes object
+        public string CaptionLink { get; set; }
+        public string NameInfo { get; set; }
+        public string InfoDetails { get; set; }
+        public string InfoPrivat { get; set; }
+
+        //Money
+        public int GrnPrice { get; set; }
+        public int DollarPrice { get; set; }
 
     }
 }
