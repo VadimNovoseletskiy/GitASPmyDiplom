1a44334102624c8e11b2f3b20585054704179bfd
Master <ttm121212@gmail.com>
2017-10-15 19:22:23 +0300

    Work on Veiw (Index)ApartmentInputController and class"ApartmentInputParameters".

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/ApartmerntInputParameters.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/ApartmerntInputParameters.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Domains.Models;
 
 namespace NLayerApp.BusinessLogicLayer.Models
 {
     public class ApartmerntInputParameters
     {
+        public PropertyType Type = PropertyType.Apartment;
+        //Address
+        public string Village { get; set; }
+        public string Region { get; set; }
+        public string Street { get; set; }
+        public string NumberAdress { get; set; }
+        public TypeOfOperation OperationType { get; set; }
+
+        //Details Info  
 
     }
 }
