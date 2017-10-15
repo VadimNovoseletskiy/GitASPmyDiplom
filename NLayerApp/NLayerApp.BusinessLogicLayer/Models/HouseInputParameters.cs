303c9058b9bab47df7ab366138ba52abc6c354e7
Master <ttm121212@gmail.com>
2017-10-15 16:26:47 +0300

    Disable part kod "Additional Equipment" and "OutBuildings" Feature fix bug.

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/HouseInputParameters.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/HouseInputParameters.cs
@@ -31,28 +31,28 @@ namespace NLayerApp.BusinessLogicLayer.Models
         public float LandAreaHouse { get; set; }//
 
   //Additional Equipment 
-        public bool Boiler { get; set; }
-        public bool Intercom { get; set; }
-        public bool Internet { get; set; }
-        public bool CableTv { get; set; }
-        public bool FirePlace { get; set; }
-        public bool Air { get; set; }
-        public bool Furniture { get; set; }
-        public bool Signaling { get; set; }
-        public bool SatelliteTv{ get; set; }
-        public bool MyWindows { get; set; }
-        public bool Telephone { get; set; }
-        public bool WashingMachine { get; set; }
+        //public bool Boiler { get; set; }
+        //public bool Intercom { get; set; }
+        //public bool Internet { get; set; }
+        //public bool CableTv { get; set; }
+        //public bool FirePlace { get; set; }
+        //public bool Air { get; set; }
+        //public bool Furniture { get; set; }
+        //public bool Signaling { get; set; }
+        //public bool SatelliteTv{ get; set; }
+        //public bool MyWindows { get; set; }
+        //public bool Telephone { get; set; }
+        //public bool WashingMachine { get; set; }
 //OutBuildings
-        public bool BahtHouse { get; set; }
-        public bool Swimming { get; set; }
-        public bool Garage{ get; set; }
-        public bool Well{ get; set; }
-        public bool SummerKitchen{ get; set; }
-        public bool WorkShop { get; set; }
-        public bool Barn { get; set; }
-        public bool Well1 { get; set; }
-        public bool GreenHouse { get; set; }
+        //public bool BahtHouse { get; set; }
+        //public bool Swimming { get; set; }
+        //public bool Garage{ get; set; }
+        //public bool Well{ get; set; }
+        //public bool SummerKitchen{ get; set; }
+        //public bool WorkShop { get; set; }
+        //public bool Barn { get; set; }
+        //public bool Well1 { get; set; }
+        //public bool GreenHouse { get; set; }
 
 
  //communications
