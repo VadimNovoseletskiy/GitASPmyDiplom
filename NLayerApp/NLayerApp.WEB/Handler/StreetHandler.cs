051a00179ac2a24b0de5b7c72c5836a317502a72
Master <ttm121212@gmail.com>
2017-10-16 11:42:17 +0300

    Add class"StreetHandler".

---
+++ NLayerApp/NLayerApp.WEB/Handler/StreetHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+using NLayerApp.WEB.Models;
+
+namespace NLayerApp.WEB.Handler
+{
+    public class StreetHandler
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public StreetHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public StreetHandler() : this(new UnitOfWork())
+        {
+
+        }
+
+        public List<DropDownViewModel> GetStreet()
+        {
+            List<DropDownViewModel> result = this.unitOfWork.GenericRepository<Street>()
+                .Get()
+                .Select(
+                    p => new DropDownViewModel
+                    {
+                        Id = p.Id,
+                        Value = p.StreetName}
+                )
+                .ToList<DropDownViewModel>();
+            return result;
+        }
+
+    }
+}
