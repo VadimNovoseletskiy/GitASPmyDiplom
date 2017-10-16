b26f09e40377f6d5c0b802c54b97eebf2580716e
Master <ttm121212@gmail.com>
2017-10-16 11:03:24 +0300

    Add in class(entity) "House"  property from class (entity)"OutBuilding".

--- NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/House.cs
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/House.cs
@@ -92,6 +92,87 @@ namespace NLayerApp.DataAccessLayer.Domains.Models
         /// </value>
         public float LandAreaHouse { get; set; }
 
+        /// <summary>
+        /// The property BahtHouseOutBuilding 
+        /// </summary>
+        ///<value>
+        /// The Bathhouse 
+        ///</value>
+
+        public bool BahtHouseOutBuilding { get; set; }
+
+
+        /// <summary>
+        /// The property SwimmingOutBuildings
+        /// </summary>
+        /// <value>
+        /// The swimming-pool 
+        /// </value>
+        public bool SwimmingOutBuildings { get; set; }
+
+
+        /// <summary>
+        /// The property GarageOutBuildings 
+        /// </summary>
+        /// <value>
+        /// The Garage
+        /// </value>
+        public bool GarageOutBuildings { get; set; }
+
+
+        /// <summary>
+        /// The property WellOutBuildings 
+        /// </summary>
+        /// <value>
+        /// The  Well
+        /// </value>
+        public bool WellOutBuildings { get; set; }
+
+
+        /// <summary>
+        /// The property SummerKitchenOutBuildings
+        /// </summary>
+        /// <value>
+        /// Summer cuisine   
+        /// </value>
+        public bool SummerKitchenOutBuildings { get; set; }
+
+
+        /// <summary>
+        /// The property WorkShopOutBuildings
+        /// </summary>
+        /// <value>
+        /// The Workshop
+        /// </value>
+        public bool WorkShopOutBuildings { get; set; }
+
+
+        /// <summary>
+        /// The property BarnOutBuildings 
+        /// </summary>
+        /// <value>
+        /// The Barn 
+        /// </value>
+        public bool BarnOutBuildings { get; set; }
+
+
+        /// <summary>
+        /// The property Well1OutBuildings
+        /// </summary>
+        /// <value>
+        /// The Well_1
+        /// </value>
+        public bool Well1OutBuildings { get; set; }
+
+
+        /// <summary>
+        /// The property GreenHouseOutBuildings
+        /// </summary>
+        /// <value>
+        /// The Greenhouse 
+        /// </value>
+        public bool GreenHouseOutBuildings { get; set; }
+
 
         //Links: 
 
@@ -99,6 +180,9 @@ namespace NLayerApp.DataAccessLayer.Domains.Models
         ///The link to the Parent Class"Info"(one to one communication) 
         /// </summary>
         public Info Info { get; set; }
+
+       // public OutBuilding OutBuilding { get; set; }
+
     }
 
    
