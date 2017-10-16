a0a278c7dbc9865f7743b631d7233de0baac20d5
Master <ttm121212@gmail.com>
2017-10-17 00:33:29 +0300

    Add "DiktionaryRegionController" and his View.

---
+++ NLayerApp/NLayerApp.WEB/Controllers/DictionatyRegionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NLayerApp.WEB.Controllers
+{
+    public class DictionatyRegionController : Controller
+    {
+        // GET: DictionatyRegion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
