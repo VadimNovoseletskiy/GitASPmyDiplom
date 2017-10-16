789fca06a4a932ac78a7be323d22d805462f8b32
Master <ttm121212@gmail.com>
2017-10-16 19:17:03 +0300

    Add Controller and Hantler for  Region .

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Handler/DictunaryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.BusinessLogicLayer.Handler
+{
+    public class DictunaryHandler
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public DictunaryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public DictunaryHandler() : this(new UnitOfWork())
+        {
+        }
+
+        public void InsertRegion(RegionInputParameters parameters)
+        {
+            Region myRegion=new Region {RegionName = parameters.NameRegion};
+            this.unitOfWork.GenericRepository<Region>().Insert(myRegion);
+        }
+    }
+}
