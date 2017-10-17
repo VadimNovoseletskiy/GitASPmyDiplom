732f70a48f37be5065a5120059af117f3d9b7ec9
Master <ttm121212@gmail.com>
2017-10-17 22:37:07 +0300

    Work in Dictionaty Village

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Handler/DictionaryVillageHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.BusinessLogicLayer.Handler
+{
+     public class DictionaryVillageHandler
+     {
+         private readonly IUnitOfWork unitOfWork;
+
+         public DictionaryVillageHandler(IUnitOfWork unitOfWork)
+         {
+             this.unitOfWork = unitOfWork;
+         }
+
+         public DictionaryVillageHandler() : this(new UnitOfWork())
+         {
+
+         }
+        //OutPut
+         public List<VillageViewModels> GetVillages() =>
+             this.unitOfWork.GenericRepository<Village>()
+                 .Get()
+                 .Select(
+                     p => new VillageViewModels
+                     {
+                         Id = p.Id,
+                         Village = p.VillageName
+                     }
+                 )
+                 .ToList<VillageViewModels>();
+
+         public void InsertVillage(VillageInputParameters parameters)
+         {
+            Village myVillage=new Village {VillageName = parameters.Village};
+            this.unitOfWork.GenericRepository<Village>().InsertPhoto(myVillage);
+            this.unitOfWork.SaveChanges();
+         }
+
+         public Village FindVillage(object id) =>
+             this.unitOfWork.GenericRepository<Village>().Find(id);
+
+         public void UpdateViillage(Village village)
+         {
+            this.unitOfWork.GenericRepository<Village>().Update(village);
+            this.unitOfWork.SaveChanges();
+         }
+     }
+}
---
+++ NLayerApp/NLayerApp.WEB/Controllers/DictionaryVillageController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NLayerApp.BusinessLogicLayer.Handler;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.WEB.Controllers
+{
+    public class DictionaryVillageController : Controller
+    {
+        IUnitOfWork unitOfWork=new UnitOfWork();
+
+        // GET: DictionaryVillage
+        [HttpGet]
+        public ActionResult Index()
+        {
+            DictionaryVillageHandler myDictionaryVillageHandler=new DictionaryVillageHandler(unitOfWork);
+            var result = myDictionaryVillageHandler.GetVillages();
+
+            return View(result);
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Index(VillageInputParameters parameters)
+        {
+            DictionaryVillageHandler myDictionaryVillageHandler = new DictionaryVillageHandler(unitOfWork);
+            myDictionaryVillageHandler.InsertVillage(parameters);
+            return RedirectToAction("Index");
+        }
+
+        //Get
+        [HttpGet]
+        public ActionResult Edit(int ? id)
+        {
+            if (id==null)
+            {
+                return new  HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            DictionaryVillageHandler myDictionaryVillageHandler = new DictionaryVillageHandler(unitOfWork);
+
+            var result = myDictionaryVillageHandler.FindVillage(id);
+            if (result==null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Village village)
+        {
+            DictionaryVillageHandler myDictionaryVillageHandler = new DictionaryVillageHandler(unitOfWork);
+            if (ModelState.IsValid)
+            {
+                myDictionaryVillageHandler.UpdateViillage(village);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
