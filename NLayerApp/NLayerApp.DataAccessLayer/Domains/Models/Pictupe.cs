2b11d60c90ef87f3808ce36a37a30d4131827b13
Master <ttm121212@gmail.com>
2017-12-17 23:11:37 +0200

    Add class "Picture "

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Pictupe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.DataAccessLayer.Domains.Models
+{
+    public class Pictupe
+    {
+        public int Id { get; set; }
+        public string Name { get; set;}
+        public byte[] Image { get; set; }
+    }
+}
