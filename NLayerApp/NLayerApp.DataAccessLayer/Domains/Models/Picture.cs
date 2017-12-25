fd49ebacc86abdd9c4fe945e1564b8863842267f
Master <ttm121212@gmail.com>
2017-12-25 21:27:54 +0200

    Add class "Picture"

---
+++ NLayerApp/NLayerApp.DataAccessLayer/Domains/Models/Picture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.DataAccessLayer.Domains.Models
+{
+    public class Picture
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set;}
+        public byte[] Image { get; set; }
+
+       
+
+        //Links
+
+        /// <summary>
+        /// The foreign key
+        /// The link to the class "Info"(one to many communication)
+        /// </summary>
+        public int? InfoId { get; set; }
+        public Info Info { get; set; }
+    }
+}
