ec0c73f42584f45f30f07d9cc5f1da79f83151be
Master <ttm121212@gmail.com>
2018-01-08 22:09:56 +0200

    Add class "ImaageParameters"

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/ImageParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BusinessLogicLayer.Models
+{
+    public class ImageParameters
+    {
+        public int Id { get; set; }
+        public string fotoName { get; set; }
+        public byte[] Image { get; set; }
+
+        public int InfoId { get; set; }
+    }
+}
