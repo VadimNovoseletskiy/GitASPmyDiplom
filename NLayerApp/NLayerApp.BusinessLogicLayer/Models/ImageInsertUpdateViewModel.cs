048c8d5ae8bca0b8e07155380a485dad2699bd2b
Master <ttm121212@gmail.com>
2018-01-08 22:10:24 +0200

    Add class "ImageInsertUpdateViewModel"

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/ImageInsertUpdateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.DataAccessLayer.Domains.Models;
+
+namespace NLayerApp.BusinessLogicLayer.Models
+{
+    public class ImageInsertUpdateViewModel
+    {
+        /*выаод дополнительной информации для подтвержления  ввода картинки выбранному обьтекту  */
+
+       
+
+        //image
+        public int IdObject { get; set; }
+        public int InfoId { get; set; }
+        public string fotoName { get; set; }
+        public byte[] Image { get; set; }
+
+    }
+}
