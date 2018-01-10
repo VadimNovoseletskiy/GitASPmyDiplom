69e5e40f9f56fc6aa46cbf472b1756a2318abe78
Master <ttm121212@gmail.com>
2018-01-10 23:13:31 +0200

    Add class "DeleteImageViewModel"

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/DeleteImageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NLayerApp.BusinessLogicLayer.Models
+{
+    public class DeleteImageViewModel
+    {
+        public int Id { get; set; }
+        public string fotoName { get; set; }
+        public byte[] Image { get; set; }
+        public int InfoId { get; set; }
+    }
+}
