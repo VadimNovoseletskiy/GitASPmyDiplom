47da76d2e594cf50e5923f33d244e201265b55fd
Master <ttm121212@gmail.com>
2018-01-10 23:16:33 +0200

    Fix suntax error in class "ImageInsertUpdateViewModel"

--- NLayerApp/NLayerApp.BusinessLogicLayer/Models/ImageInsertUpdateViewModel.cs
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/ImageInsertUpdateViewModel.cs
@@ -14,7 +14,7 @@ namespace NLayerApp.BusinessLogicLayer.Models
        
 
         //image
-        public int IdObject { get; set; }
+        public int IdPicture { get; set; }
         public int InfoId { get; set; }
         public string fotoName { get; set; }
         public byte[] Image { get; set; }
