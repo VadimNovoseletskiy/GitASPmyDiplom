f9b1b3805294251c1fbf8b24b0644dfbf00fcbc6
Master <ttm121212@gmail.com>
2018-01-11 22:38:49 +0200

    Add class "AllImageViewModel" in the BusinessLogicLayer.

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Models/DeleteAllimageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BusinessLogicLayer.Models
+{
+    public class DeleteAllimageViewModel
+    {
+        public int IdPicture { get; set; }
+        public int InfoId { get; set; }
+    }
+}
