502a461ff6c0f020c40019dd5c8b9e44dea426fb
Master <ttm121212@gmail.com>
2018-01-15 22:45:37 +0200

    Add in class "AdministratorController" function "GetAllObject".

--- NLayerApp/NLayerApp.WEB/Controllers/AdministratorController.cs
+++ NLayerApp/NLayerApp.WEB/Controllers/AdministratorController.cs
@@ -7,6 +7,7 @@ using NLayerApp.BusinessLogicLayer.Handler;
 using NLayerApp.BusinessLogicLayer.Models;
 using NLayerApp.DataAccessLayer.Interface;
 using NLayerApp.DataAccessLayer.Repository;
+using NLayerApp.WEB.Handler;
 
 namespace NLayerApp.WEB.Controllers
 {
@@ -19,7 +20,43 @@ namespace NLayerApp.WEB.Controllers
         {
             return View();
         }
-        
-        
+
+        public ActionResult AllObject()
+        {
+            MySelect();
+            AdministratorHandler myHandler=new AdministratorHandler(unitOfWork);
+            var result = myHandler.GetAllObject();
+
+            return View(result);
+        }
+
+        void MySelect()
+        {
+            RegionHandler myRegionHandler = new RegionHandler(unitOfWork);
+            var resultRegion = myRegionHandler.GetRegion();
+            SelectList listRegion = new SelectList(resultRegion, "Id", "Value");
+            ViewBag.regionName = listRegion;
+
+            VillageHandler myVillageHandler = new VillageHandler(unitOfWork);
+            var resultVillage = myVillageHandler.GetVillage();
+            SelectList listVillage = new SelectList(resultVillage, "Id", "Value");
+            ViewBag.villageName = listVillage;
+
+            StreetHandler myStreetHandler = new StreetHandler(unitOfWork);
+            var resultStreet = myStreetHandler.GetStreet();
+            SelectList listStreet = new SelectList(resultStreet, "Id", "Value");
+            ViewBag.streetName = listStreet;
+
+            FloorMaterialHandler myFloorMaterialHandler = new FloorMaterialHandler(unitOfWork);
+            var resultFloorMaterial = myFloorMaterialHandler.GetFloorMaterial();
+            SelectList listFoorMaterial = new SelectList(resultFloorMaterial, "Id", "Value");
+            ViewBag.floorMaterialName = listFoorMaterial;
+
+            WallMaterialHandler myWallMaterialHandler = new WallMaterialHandler(unitOfWork);
+            var resultWallMaterial = myWallMaterialHandler.GetWallMaterial();
+            SelectList listWallMaterial = new SelectList(resultWallMaterial, "Id", "Value");
+            ViewBag.wallMaterialName = listWallMaterial;
+        }
+
     }
 }
