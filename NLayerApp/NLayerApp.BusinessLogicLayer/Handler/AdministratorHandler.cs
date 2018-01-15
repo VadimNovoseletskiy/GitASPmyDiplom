6187f931e36bba08b27a459294976959841922c7
Master <ttm121212@gmail.com>
2018-01-15 22:47:50 +0200

    Add class "AdministratorHandler" in Business Layer

---
+++ NLayerApp/NLayerApp.BusinessLogicLayer/Handler/AdministratorHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.BusinessLogicLayer.Models;
+using NLayerApp.DataAccessLayer.Domains.Models;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.BusinessLogicLayer.Handler
+{
+    public class AdministratorHandler
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public AdministratorHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public AdministratorHandler() : this(new UnitOfWork())
+        {
+
+        }
+
+        public List<AdminAllObjectViewModel> GetAllObject() =>
+            this.unitOfWork.GenericRepository<Info>()
+                .Get()
+                .Where(s => s.Type == PropertyType.Apartment
+                            || s.Type == PropertyType.Commercial
+                            || s.Type == PropertyType.House
+                            || s.Type == PropertyType.Land)
+                .Select(
+                    s => new AdminAllObjectViewModel
+                    {
+                        Id = s.Id,
+                        OperationType = s.OperationType,
+                        Village = s.Village.VillageName,
+                        Street = s.Street.StreetName,
+                        AdressNumber = s.AddressNumber
+                    }
+                )
+                .ToList<AdminAllObjectViewModel>();
+    }
+}
