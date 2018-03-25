6e9401bd3636e894bbaf83256e4abfc9449a0374
Master <ttm121212@gmail.com>
2018-03-25 17:25:26 +0300

    Add class"ImageController"

---
+++ NLayerApp/NLayerApp.WEB/Controllers/ImageController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NLayerApp.BusinessLogicLayer.Handler;
+using NLayerApp.DataAccessLayer.Interface;
+using NLayerApp.DataAccessLayer.Repository;
+
+namespace NLayerApp.WEB.Controllers
+{
+    public class ImageController : Controller
+    {
+        IUnitOfWork unitOfWork=new UnitOfWork();
+        // GET: Thumbnail
+        public ActionResult Thumbnail(int ? id )
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            GetThumpnailHandler myHandler=new GetThumpnailHandler(unitOfWork);
+            var result = myHandler.Execute(id);
+
+            if (result == null)
+            {
+                return HttpNotFound();
+            }
+
+            return File(result.Image, MimeMapping.GetMimeMapping(result.Name), result.Name);
+        }
+
+        // GET: Index
+        public ActionResult Index(int ? id )
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var myHandler= new GetImageHandler(unitOfWork);
+            var result = myHandler.Execute(id);
+
+            if (result == null)
+            {
+                return HttpNotFound();
+            }
+
+            return File(result.Image, MimeMapping.GetMimeMapping(result.Name), result.Name);
+        }
+    }
+}
