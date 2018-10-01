94d7211eade57d6c7cd1521b143deeee9de4fb1b
Stalker <ttm121212@gmail.com>
2018-10-01 14:31:05 +0300

    Work on the fronted.Adaptive menu on size (280-479px) and (1199-1379px) for pages : index, "послуги", "кредитуваня", "контактна інформація", "про нас", form search :"квартири","буднки","комерційна нерухомість","земельні ділянки".Result search:"Квартири".

---
+++ NLayerApp/NLayerApp.WEB/Scripts/sliderText.js
@@ -0,0 +1,11 @@
+﻿function SelectedOperation(a) {
+    var label = a.value;
+    if (label == "оренда") {
+        document.getElementById("grn").style.display = 'inline-block';
+        document.getElementById("dol").style.display = 'none';
+    }
+    else {
+        document.getElementById("dol").style.display = 'inline-block';
+        document.getElementById("grn").style.display = 'none';
+    }
+}
